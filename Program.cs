258a7b73860e18d8edb40b35df0d4bbd3c308aea
Davi <davicamiloribeiro@gmail.com>
2022-04-25 18:20:53 -0300

    Teste de base

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Fundamentos{
+    class Program{
+        static void Main(string[] args){
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+            
+
+            float resultado = v1 + v2;
+            Console.WriteLine("Resultado: "+resultado);
+        }
+    }
+}
